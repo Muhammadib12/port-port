1ced6275cf035bbd7d09a29551e3fbb53886e802
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-03-03 15:09:18 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Recomanded from "./components/Recomanded.jsx";
 
 function App() {
   return (
-    <div className="overflow-x-hidden mt-20 text-neutral-300 antialiased selection:bg-green-300 selection:text-black">
+    <div className="overflow-x-hidden  text-neutral-300 antialiased selection:bg-green-300 selection:text-black">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
